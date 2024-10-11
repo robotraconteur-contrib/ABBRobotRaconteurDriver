c67239be2e0998afec82590749e7f4f62885ebdd
John Wason <wason@wasontech.com>
2024-10-11 13:28:49 -0400

    Add examples

---
+++ examples/abb_jog_example_client.py
@@ -0,0 +1,35 @@
+from RobotRaconteur.Client import *
+import time
+import numpy as np
+
+c = RRN.ConnectService('rr+tcp://localhost:58651?service=robot')
+
+robot_info = c.robot_info
+print(robot_info)
+
+print(c.robot_state.PeekInValue()[0].command_mode)
+
+robot_const = RRN.GetConstants("com.robotraconteur.robotics.robot", c)
+
+halt_mode = robot_const["RobotCommandMode"]["halt"]
+jog_mode = robot_const["RobotCommandMode"]["jog"]
+
+c.command_mode = halt_mode
+time.sleep(0.1)
+c.command_mode = jog_mode
+
+joint_diff = np.array([-5,0,0,0,0,5,0])*np.deg2rad(1)
+joint_vel = np.array([5.0,1,1,1,1,1])
+
+for i in range(5):
+    t = time.time()
+    print("Begin move")
+    c.jog_freespace(np.deg2rad([15,0,0,0,0,0]), joint_vel, True)
+    print("done")
+    time.sleep(0.5)
+    print(hex(c.robot_state.PeekInValue()[0].robot_state_flags))
+    print("Begin move")
+    c.jog_freespace(np.deg2rad([-15,0,0,0,0,0]), joint_vel, True)
+    print("Done")
+    print(hex(c.robot_state.PeekInValue()[0].robot_state_flags))
+    time.sleep(0.5)
---
+++ examples/abb_trajectory_command_example.py
@@ -0,0 +1,90 @@
+from RobotRaconteur.Client import *
+import time
+import numpy as np
+
+c = RRN.ConnectService('rr+tcp://localhost:58651?service=robot')
+
+robot_info = c.robot_info
+print(robot_info)
+
+print(c.robot_state.PeekInValue()[0].command_mode)
+
+robot_const = RRN.GetConstants("com.robotraconteur.robotics.robot", c)
+
+joint_names = [j.joint_identifier.name for j in robot_info.joint_info]
+
+halt_mode = robot_const["RobotCommandMode"]["halt"]
+trajectory_mode = robot_const["RobotCommandMode"]["trajectory"]
+
+JointTrajectoryWaypoint = RRN.GetStructureType("com.robotraconteur.robotics.trajectory.JointTrajectoryWaypoint",c)
+JointTrajectory = RRN.GetStructureType("com.robotraconteur.robotics.trajectory.JointTrajectory",c)
+
+c.command_mode = halt_mode
+time.sleep(0.1)
+c.command_mode = trajectory_mode
+
+state_w = c.robot_state.Connect()
+
+state_w.WaitInValueValid()
+state1 = state_w.InValue
+
+waypoints = []
+
+j_start = state1.joint_position
+j_end = [0, 0, 0, 0, 0, 0]
+
+for i in range(11):
+    wp = JointTrajectoryWaypoint()
+    wp.joint_position = (j_end - j_start)*(float(i)/10.0) + j_start
+    wp.time_from_start = i/2.0
+    waypoints.append(wp)
+
+traj = JointTrajectory()
+traj.joint_names = joint_names
+traj.waypoints = waypoints
+
+c.speed_ratio = 1
+
+traj_gen = c.execute_trajectory(traj)
+
+while (True):
+    t = time.time()
+
+    robot_state = state_w.InValue
+
+    res, gen_state = traj_gen.TryNext()
+    if not res:
+        break
+    
+    print(gen_state)
+    print(hex(c.robot_state.PeekInValue()[0].robot_state_flags))
+
+waypoints = []
+
+for i in range(101):
+    t = float(i)/10.0
+    wp = JointTrajectoryWaypoint()
+    cmd = np.deg2rad(15)*np.sin(2*np.pi*(t/10.0))*np.array([1,0,0,0,0.5,-1])
+    cmd = cmd + j_end
+    wp.joint_position = cmd
+    wp.time_from_start = t
+    waypoints.append(wp)
+
+traj = JointTrajectory()
+traj.joint_names = joint_names
+traj.waypoints = waypoints
+
+c.speed_ratio = 1.0
+
+traj_gen = c.execute_trajectory(traj)
+
+while (True):
+    t = time.time()
+
+    robot_state = state_w.InValue
+    
+    res, gen_state = traj_gen.TryNext()
+    if not res:
+        break
+    
+    print(gen_state)
